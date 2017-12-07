d0351acd0e910723d1b8bc12a0741a918d75e91d
Declan John Ashley <DECLAN.ASHLEY.2014@mumail.ie>
2017-12-07 18:31:25 +0000

    Just adding some basic work on the playlist iframe

--- static/js/main.js
+++ static/js/main.js
@@ -30,7 +30,6 @@ function getGraph(){
       console.log(apiresponse)
       console.log(id);
 
-
       ////////////////Slideshow//////////////////////
     DZ.api('/user/me/tracks', function(response){
     for (var i = 0; i <3; i++) {
@@ -38,10 +37,27 @@ function getGraph(){
     output = response.data[i].album.cover_xl;
     document.getElementById("pic"+i).innerHTML="<img src=\"" + output + "\">";
     }
-
-
-
   });
+
+  
+  // Here I just call the api for a user's playlist
+  DZ.api('/user/me/playlists', function(response){
+  //store response
+  var id = response.data[1].id;
+  //add it to the iframe link
+  var link = "https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=007FEB&layout=dark&size=medium&type=playlist&id=" + id + "&app_id=1"
+   var iframe = document.createElement('iframe');
+   iframe.frameBorder=0;
+   iframe.width="450px";
+   iframe.height="250px";
+   iframe.id="randomid";
+   iframe.setAttribute("src", link);
+   document.getElementById("play").appendChild(iframe);
+   
+  });
+
+
+
 //    // no user session available, someone you dont know
 //  }
 // });
